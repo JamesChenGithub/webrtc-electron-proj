211094c0fa4d41c947033b33a2f57856f161efe5
AlexiChen <>
2019-04-15 14:42:32 +0800

    添加基本样式

---
+++ main.js
@@ -0,0 +1,49 @@
+const {app, BrowserWindow} = require('electron');
+const url = require('url');
+const path = require('path');
+// const pdf = require('./main-process/pdf.js').pdf;
+
+let mainWindow;
+
+const createWindow = ()=>{
+    mainWindow = new BrowserWindow({
+        width : 800,
+        height : 600
+    });
+
+    const indexpath = url.format({
+        pathname : path.join(__dirname, 'index.html'),
+        protocol : 'file:',
+        slashes : true
+    });
+
+    mainWindow.loadURL(indexpath);
+    mainWindow.webContents.openDevTools(false);
+
+    mainWindow.on('closed', ()=>{
+        mainWindow = null;
+    });
+
+    // require('./main-process/menu.js');
+    // require('./main-process/msg-a.js');
+    // require('./main-process/tray.js');
+    // require('./main-process/dialog.js');
+
+    // pdf(mainWindow);
+    
+}
+
+// require('./main-process/shortcut.js');
+app.on('ready', createWindow);
+
+app.on('windows-all-closed', ()=>{
+    if(process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', ()=>{
+    if(mainWindow === null) {
+        createWindow();
+    }
+});
